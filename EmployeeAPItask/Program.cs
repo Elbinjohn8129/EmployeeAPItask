7d3026c8ab9388fabed7bc3f76357fda8e63fdea
elbin <elbinjohn8129@gmail.com>
2026-01-15 23:36:22 +0530

    updated logic in repo

--- EmployeeAPItask/Program.cs
+++ EmployeeAPItask/Program.cs
@@ -1,27 +1,56 @@
-namespace EmployeeAPItask
+using EmployeeAPI.Repository;
+using EmployeeAPI.Services;
+using EmployeeAPItask.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// "DefaultConnection"
+builder.Services.AddDbContext<EmployeeApiContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+// Repository + Service 
+builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+
+// Enable CORS 
+builder.Services.AddCors(options =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+    options.AddPolicy("AllowAll",
+        policy => policy.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+});
 
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
+var app = builder.Build();
 
 
-            app.MapControllers();
-
-            app.Run();
-        }
-    }
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+// Enable HTTPS redirection
+app.UseHttpsRedirection();
+
+// Enable CORS
+app.UseCors("AllowAll");
+
+// Enable authorization (if needed later)
+app.UseAuthorization();
+
+// Map controller routes
+app.MapControllers();
+
+// Run the app
+app.Run();
