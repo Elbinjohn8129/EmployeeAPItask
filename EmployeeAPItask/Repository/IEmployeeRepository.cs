a7cb30dce97b94edd5ad25661f5f337e8cb88d60
elbin <elbinjohn8129@gmail.com>
2026-01-15 23:36:22 +0530

    updated logic in repo

--- EmployeeAPItask/Repository/IEmployeeRepository.cs
+++ EmployeeAPItask/Repository/IEmployeeRepository.cs
@@ -1,6 +1,15 @@
-﻿namespace EmployeeAPItask.Repository
+﻿
+using EmployeeAPI.ViewModels;
+using EmployeeAPItask.Models;
+
+namespace EmployeeAPI.Repository
 {
     public interface IEmployeeRepository
     {
+        Task<List<Employee>> GetAll();
+        Task<Employee> GetById(int id);
+        Task<Employee> Add(EmployeeViewModel dto);
+        Task<Employee> Update(int id, EmployeeViewModel dto);
+        Task<bool> Delete(int id);
     }
 }
